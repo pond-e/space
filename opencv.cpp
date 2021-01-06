8abd6fdad7c9fc9b03088631fd14c718f6bb6de3
pond-e <haruto2266@icloud.com>
2021-01-06 13:20:40 +0900

    change

--- opencv.cpp
+++ opencv.cpp
@@ -1,6 +1,8 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/imgproc/types_c.h>
+#include <iostream>
 using namespace cv;
+using namespace std;
 int main(){
     Mat img = imread("root.jpg", IMREAD_UNCHANGED);
     Mat gray_img;
